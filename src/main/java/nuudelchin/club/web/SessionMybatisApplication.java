2af76b49942f79f21043f0c819343214b3346194
lkhaajav <lkhaching@gmail.com>
2024-11-24 20:59:04 +0900

    init code

---
+++ src/main/java/nuudelchin/club/web/SessionMybatisApplication.java
@@ -0,0 +1,13 @@
+package nuudelchin.club.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionMybatisApplication.class, args);
+	}
+
+}
